9db655ff23b23fa7a6ae2cb2eb09195bf61ae46f
phunghung <phunghung0901xx@gmail.com>
2022-10-08 23:35:57 +0700

    first commit

---
+++ pages/_app.js
@@ -0,0 +1,16 @@
+import '../styles/index.scss'
+import Nav from '../components/Nav/Nav'
+
+function MyApp({ Component, pageProps }) {
+
+  return ( 
+    <div className='app_wrapper'>
+      <Nav />
+ <Component {...pageProps} />
+
+    </div>
+  
+  )
+}
+
+export default MyApp
